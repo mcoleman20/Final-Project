e3a24a7dd3b484a318cfc5b2f33521ab81def5ed
mcoleman20 <59784147+mcoleman20@users.noreply.github.com>
2020-05-02 22:03:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload=function(){
+  document.getElementById("linkid").click();
+};
+
+function doSomething(){
+  alert("Introduction to Web development ( design HCI and creation) using current standards for client-side content deliver (e.g., HTML and CSS). Students will learn to create and publish a multi-page, static-content website using associated applications. Special focus is given to user interface design, data presentation, and data organization. Prerequisites: CIS 225, 236 (with a grade of C or higher in both). (Fall, Spring; Summer on sufficient demand)")
+}
+
+
+window.onload=function(){
+  document.getElementById("des2").click();
+};
+
+function link(){
+  alert("Introduction to Web development (design and creation) using current standards for client-side content deliverh (e.g., HTML and CSWS). Students will learn to create and publish a multi-page, static-content website using associated applications. Special focus is given to user interface design, data presentation, and data organization. Prerequisites: CIS 225, 236 (with a grade of C or higher in both).")
+}
+
+window.onload=function(){
+  document.getElementById("des3").click();
+};
+
+function link2(){
+  alert("Introduction to Web development (design and creation) using current standards for client-side content deliverh (e.g., XHTML and CSWS). Students will learn to create and publish a multi-page, static-content website using associated applications. Special focus is given to user interface design, data presentation, and data organization. Prerequisites: CIS 225, 236 (with a grade of C or higher in both).")
+}
